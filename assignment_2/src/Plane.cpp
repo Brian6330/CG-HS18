645124df33e8debbd65909d2c63a46c0248a9c93
joeftiger <j.oeftiger@protonmail.com>
2018-10-09 17:07:43 +0200

    Copied over our solutions for Cylinder and Plane

--- assignment_2/src/Plane.cpp
+++ assignment_2/src/Plane.cpp
@@ -12,7 +12,10 @@
 //== INCLUDES =================================================================
 
 #include "Plane.h"
+#include "SolveQuadratic.h"
+
 #include <limits>
+#include <array>
 
 
 //== CLASS DEFINITION =========================================================
@@ -35,10 +38,32 @@ intersect(const Ray& _ray,
           vec3&      _intersection_normal,
           double&    _intersection_t ) const
 {
+    /** \todo
+     * - compute the intersection of the plane with `_ray`
+     * - if ray and plane are parallel there is no intersection
+     * - otherwise compute intersection data and store it in `_intersection_point`, `_intersection_normal`, and `_intersection_t`.
+     * - return whether there is an intersection in front of the viewer (t > 0)
+    */
 
-    // \todo Copy your assignment 1 solution here.
+    // SELF-IMPLEMENTATION
+    const vec3    &dir = _ray.direction;
+    const vec3 &origin = _ray.origin;
+    const vec3  offset = center - origin;
 
-    return false;
+    const double angle = dot(normal, dir);
+
+    // is perpendicular (very close to)
+    if (std::abs(angle) == 0) return false;
+
+    _intersection_t = dot(normal, offset) / angle;
+
+    // intersection behind the viewer;
+    if (_intersection_t <= 0) return false;
+
+    _intersection_point = origin + _intersection_t*dir;
+    _intersection_normal = normal;
+
+    return true;
 }
 
 
