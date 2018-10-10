88dd0eef01d83f7ec4562c97d3a68c82da7f93f3
joeftiger <j.oeftiger@protonmail.com>
2018-10-11 00:39:36 +0200

    Added ambient, diffuse, specular according to formula. Results look strange (wrong)

--- assignment_2/src/Scene.cpp
+++ assignment_2/src/Scene.cpp
@@ -30,33 +30,33 @@
 
 Image Scene::render()
 {
-    // allocate new image.
-    Image img(camera.width, camera.height);
+	// allocate new image.
+	Image img(camera.width, camera.height);
 
-    // Function rendering a full column of the image
-    auto raytraceColumn = [&img, this](int x) {
-        for (int y=0; y<int(camera.height); ++y)
-        {
-            Ray ray = camera.primary_ray(x,y);
+	// Function rendering a full column of the image
+	auto raytraceColumn = [&img, this](int x) {
+		for (int y=0; y<int(camera.height); ++y)
+		{
+			Ray ray = camera.primary_ray(x,y);
 
-            // compute color by tracing this ray
-            vec3 color = trace(ray, 0);
+			// compute color by tracing this ray
+			vec3 color = trace(ray, 0);
 
-            // avoid over-saturation
-            color = min(color, vec3(1, 1, 1));
+			// avoid over-saturation
+			color = min(color, vec3(1, 1, 1));
 
-            // store pixel color
-            img(x,y) = color;
-        }
-    };
+			// store pixel color
+			img(x,y) = color;
+		}
+	};
 
-    // If possible, raytrace image columns in parallel. We use TBB if available
-    // and try OpenMP otherwise. Note that OpenMP only works on the latest
-    // clang compilers, so macOS users will probably have the best luck with TBB.
-    // You can install TBB with MacPorts/Homebrew, or from Intel:
-    // https://github.com/01org/tbb/releases
+	// If possible, raytrace image columns in parallel. We use TBB if available
+	// and try OpenMP otherwise. Note that OpenMP only works on the latest
+	// clang compilers, so macOS users will probably have the best luck with TBB.
+	// You can install TBB with MacPorts/Homebrew, or from Intel:
+	// https://github.com/01org/tbb/releases
 #if HAS_TBB
-    tbb::parallel_for(tbb::blocked_range<int>(0, camera.width), [&raytraceColumn](const tbb::blocked_range<int> &range) {
+	tbb::parallel_for(tbb::blocked_range<int>(0, camera.width), [&raytraceColumn](const tbb::blocked_range<int> &range) {
         for (size_t i = range.begin(); i < range.end(); ++i)
             raytraceColumn(i);
     });
@@ -64,124 +64,146 @@ Image Scene::render()
 #if defined(_OPENMP)
 #pragma omp parallel for
 #endif
-    for (int x=0; x<int(camera.width); ++x)
-        raytraceColumn(x);
+	for (int x=0; x<int(camera.width); ++x)
+		raytraceColumn(x);
 #endif
 
-    // Note: compiler will elide copy.
-    return img;
+	// Note: compiler will elide copy.
+	return img;
 }
 
 //-----------------------------------------------------------------------------
 
 vec3 Scene::trace(const Ray& _ray, int _depth)
 {
-    // stop if recursion depth (=number of reflection) is too large
-    if (_depth > max_depth) return vec3(0,0,0);
+	// stop if recursion depth (=number of reflection) is too large
+	if (_depth > max_depth) return vec3(0,0,0);
 
-    // Find first intersection with an object. If an intersection is found,
-    // it is stored in object, point, normal, and t.
-    Object_ptr  object;
-    vec3        point;
-    vec3        normal;
-    double      t;
-    if (!intersect(_ray, object, point, normal, t))
-    {
-        return background;
-    }
+	// Find first intersection with an object. If an intersection is found,
+	// it is stored in object, point, normal, and t.
+	Object_ptr  object;
+	vec3        point;
+	vec3        normal;
+	double      t;
+	if (!intersect(_ray, object, point, normal, t))
+	{
+		return background;
+	}
 
-    // compute local Phong lighting (ambient+diffuse+specular)
-    vec3 color = lighting(point, normal, -_ray.direction, object->material);
+	// compute local Phong lighting (ambient+diffuse+specular)
+	vec3 color = lighting(point, normal, -_ray.direction, object->material);
 
 
-    /** \todo
-     * Compute reflections by recursive ray tracing:
-     * - check whether `object` is reflective by checking its `material.mirror`
-     * - check recursion depth
-     * - generate reflected ray, compute its color contribution, and mix it with
-     * the color computed by local Phong lighting (use `object->material.mirror` as weight)
-     * - check whether your recursive algorithm reflects the ray `max_depth` times
-     */
+	/** \todo
+	 * Compute reflections by recursive ray tracing:
+	 * - check whether `object` is reflective by checking its `material.mirror`
+	 * - check recursion depth
+	 * - generate reflected ray, compute its color contribution, and mix it with
+	 * the color computed by local Phong lighting (use `object->material.mirror` as weight)
+	 * - check whether your recursive algorithm reflects the ray `max_depth` times
+	 */
 
-    return color;
+	return color;
 }
 
 //-----------------------------------------------------------------------------
 
 bool Scene::intersect(const Ray& _ray, Object_ptr& _object, vec3& _point, vec3& _normal, double& _t)
 {
-    double  t, tmin(Object::NO_INTERSECTION);
-    vec3    p, n;
+	double  t, tmin(Object::NO_INTERSECTION);
+	vec3    p, n;
 
-    for (const auto &o: objects) // for each object
-    {
-        if (o->intersect(_ray, p, n, t)) // does ray intersect object?
-        {
-            if (t < tmin) // is intersection point the currently closest one?
-            {
-                tmin = t;
-                _object = o.get();
-                _point  = p;
-                _normal = n;
-                _t      = t;
-            }
-        }
-    }
+	for (const auto &o: objects) // for each object
+	{
+		if (o->intersect(_ray, p, n, t)) // does ray intersect object?
+		{
+			if (t < tmin) // is intersection point the currently closest one?
+			{
+				tmin = t;
+				_object = o.get();
+				_point  = p;
+				_normal = n;
+				_t      = t;
+			}
+		}
+	}
 
-    return (tmin != Object::NO_INTERSECTION);
+	return (tmin != Object::NO_INTERSECTION);
 }
 
 vec3 Scene::lighting(const vec3& _point, const vec3& _normal, const vec3& _view, const Material& _material)
 {
+	/** \todo
+	* Compute the Phong lighting:
+	* - start with global ambient contribution
+	* - for each light source (stored in vector `lights`) add diffuse and specular contribution
+	* - only add diffuse and specular light if object is not in shadow
+	*
+	* You can look at the classes `Light` and `Material` to check their attributes. Feel free to use
+	* the existing vector functions in vec3.h e.g. mirror, reflect, norm, dot, normalize
+	*/
 
-     /** \todo
-     * Compute the Phong lighting:
-     * - start with global ambient contribution
-     * - for each light source (stored in vector `lights`) add diffuse and specular contribution
-     * - only add diffuse and specular light if object is not in shadow
-     *
-     * You can look at the classes `Light` and `Material` to check their attributes. Feel free to use
-     * the existing vector functions in vec3.h e.g. mirror, reflect, norm, dot, normalize
-     */
+	const vec3 ambient_component = ambience * _material.ambient;
 
-    // visualize the normal as a RGB color for now.
-    vec3 color = (_normal + vec3(1)) / 2.0;
+	vec3 diffuse_component = vec3(0, 0, 0);
+	vec3 specular_component = vec3(0, 0, 0);
 
-    return color;
+	for (const Light &l : lights) {
+		const vec3 light_dir = _point - l.position;
+
+		// diffuse component
+		diffuse_component += l.color * _material.diffuse * std::max(0.0, dot(_normal, light_dir));
+
+		// specular component
+		const vec3 reflection = reflect(light_dir, _normal);
+		const double shine = pow(std::max(0.0, dot(reflection, _view)), _material.shininess);
+		specular_component += l.color * _material.specular * shine;
+	}
+
+
+
+	vec3 true_color = ambient_component + diffuse_component + specular_component;
+
+
+	// visualize the normal as a RGB color for now.
+	vec3 color = (_normal + vec3(1)) / 2.0;
+	color = true_color;
+
+	return color;
 }
 
 //-----------------------------------------------------------------------------
 
 void Scene::read(const std::string &_filename)
 {
-    std::ifstream ifs(_filename);
-    if (!ifs)
-        throw std::runtime_error("Cannot open file " + _filename);
+	std::ifstream ifs(_filename);
+	if (!ifs)
+		throw std::runtime_error("Cannot open file " + _filename);
 
-    const std::map<std::string, std::function<void(void)>> entityParser = {
-        {"depth",      [&]() { ifs >> max_depth; }},
-        {"camera",     [&]() { ifs >> camera; }},
-        {"background", [&]() { ifs >> background; }},
-        {"ambience",   [&]() { ifs >> ambience; }},
-        {"light",      [&]() { lights .emplace_back(ifs); }},
-        {"plane",      [&]() { objects.emplace_back(new    Plane(ifs)); }},
-        {"sphere",     [&]() { objects.emplace_back(new   Sphere(ifs)); }},
-        {"cylinder",   [&]() { objects.emplace_back(new Cylinder(ifs)); }},
-        {"mesh",       [&]() { objects.emplace_back(new     Mesh(ifs, _filename)); }}
-    };
+	const std::map<std::string, std::function<void(void)>> entityParser = {
+			{"depth",      [&]() { ifs >> max_depth; }},
+			{"camera",     [&]() { ifs >> camera; }},
+			{"background", [&]() { ifs >> background; }},
+			{"ambience",   [&]() { ifs >> ambience; }},
+			{"light",      [&]() { lights .emplace_back(ifs); }},
+			{"plane",      [&]() { objects.emplace_back(new    Plane(ifs)); }},
+			{"sphere",     [&]() { objects.emplace_back(new   Sphere(ifs)); }},
+			{"cylinder",   [&]() { objects.emplace_back(new Cylinder(ifs)); }},
+			{"mesh",       [&]() { objects.emplace_back(new     Mesh(ifs, _filename)); }}
+	};
 
-    // parse file
-    std::string token;
-    while (ifs && (ifs >> token) && (!ifs.eof())) {
-        if (token[0] == '#') {
-            ifs.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            continue;
-        }
+	// parse file
+	std::string token;
+	while (ifs && (ifs >> token) && (!ifs.eof())) {
+		if (token[0] == '#') {
+			ifs.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			continue;
+		}
 
-        if (entityParser.count(token) == 0)
-            throw std::runtime_error("Invalid token encountered: " + token);
-        entityParser.at(token)();
-    }
+		if (entityParser.count(token) == 0)
+			throw std::runtime_error("Invalid token encountered: " + token);
+		entityParser.at(token)();
+	}
 }
 
 
