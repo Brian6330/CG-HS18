59119baee861f7815a6d79a8e77d217a8e367aa5
Julius Oeftiger <j.oeftiger@protonmail.com>
2018-10-16 14:25:22 +0200

    Upload assignment 3

---
+++ assignment_3/cmake-build-debug/src/CMakeFiles/raytrace.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_3/src/Cylinder.cpp" "/mnt/c/Users/joeft/Git/CG-HS18/assignment_3/cmake-build-debug/src/CMakeFiles/raytrace.dir/Cylinder.cpp.o"
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_3/src/Mesh.cpp" "/mnt/c/Users/joeft/Git/CG-HS18/assignment_3/cmake-build-debug/src/CMakeFiles/raytrace.dir/Mesh.cpp.o"
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_3/src/Plane.cpp" "/mnt/c/Users/joeft/Git/CG-HS18/assignment_3/cmake-build-debug/src/CMakeFiles/raytrace.dir/Plane.cpp.o"
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_3/src/Scene.cpp" "/mnt/c/Users/joeft/Git/CG-HS18/assignment_3/cmake-build-debug/src/CMakeFiles/raytrace.dir/Scene.cpp.o"
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_3/src/Sphere.cpp" "/mnt/c/Users/joeft/Git/CG-HS18/assignment_3/cmake-build-debug/src/CMakeFiles/raytrace.dir/Sphere.cpp.o"
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_3/src/raytrace.cpp" "/mnt/c/Users/joeft/Git/CG-HS18/assignment_3/cmake-build-debug/src/CMakeFiles/raytrace.dir/raytrace.cpp.o"
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_3/src/vec3.cpp" "/mnt/c/Users/joeft/Git/CG-HS18/assignment_3/cmake-build-debug/src/CMakeFiles/raytrace.dir/vec3.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
