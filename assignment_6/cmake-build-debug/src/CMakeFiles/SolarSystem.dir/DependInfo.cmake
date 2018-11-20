776690bdad3ab08081fb4c2a616273b7475d4104
Julius Oeftiger <j.oeftiger@protonmail.com>
2018-11-20 10:00:45 +0100

    add pdf, remove redundant folder in folder in folder in fo....

---
+++ assignment_6/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/src/bezier.cpp" "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/bezier.cpp.o"
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/src/billboard.cpp" "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/billboard.cpp.o"
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/src/glfw_window.cpp" "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/glfw_window.cpp.o"
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/src/glmath.cpp" "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/glmath.cpp.o"
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/src/main.cpp" "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/main.cpp.o"
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/src/shader.cpp" "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/shader.cpp.o"
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/src/ship.cpp" "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/ship.cpp.o"
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/src/solar_viewer.cpp" "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/solar_viewer.cpp.o"
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/src/sphere.cpp" "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/sphere.cpp.o"
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/src/texture.cpp" "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/texture.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "GLFW_DLL"
+  "SHADER_PATH=\"/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/src\""
+  "TEXTURE_PATH=\"/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/textures\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../lib/lodePNG"
+  "../lib/glew"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/cmake-build-debug/lib/lodePNG/CMakeFiles/lodePNG.dir/DependInfo.cmake"
+  "/mnt/c/Users/joeft/Git/CG-HS18/assignment_6/cmake-build-debug/lib/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
